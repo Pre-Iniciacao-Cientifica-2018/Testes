91f452e1984f7f1ec20cb35c1b1f39fe526b5fcf
Gameleiro <30965065+JvStudent@users.noreply.github.com>
2018-12-07 17:39:24 -0200

    LoginSemBanco
    chorei mas fiz um login sem banco, vou tentar algo mais dificil
    
    Ps: com banco ta fd, mas vai sair

---
+++ Login2/app/src/main/java/com/example/etesp_2018/login/Activity2.java
@@ -0,0 +1,17 @@
+package com.example.etesp_2018.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity2 extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+
+
+
+    }
+}
---
+++ Login2/app/src/main/java/com/example/etesp_2018/login/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.etesp_2018.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnLogar = (Button) findViewById(R.id.btnLogar);
+        Button btnSair = (Button) findViewById(R.id.btnSair);
+
+
+
+        btnLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText login = (EditText) findViewById(R.id.txtLogin);
+                EditText senha = (EditText) findViewById(R.id.txtSenha);
+
+                     String loginCerto = login.getText().toString();
+                     String senhaCerta = senha.getText().toString();
+
+                if((loginCerto + "").equals( "oi" )){
+                    if(senhaCerta.equals("12")){
+                        login.setText("Certo");
+                        senha.setText("Certo");
+
+                        OpenAreaRestrita();
+
+                    }else {
+                            login.setText("qqqqqq");
+                    }
+
+
+                }else {
+                    login.setText("qqqqqw");
+                }
+            }
+
+
+
+        });
+
+
+
+    }
+    public void OpenAreaRestrita(){
+        Intent intent = new Intent(this,Activity2.class);
+        startActivity(intent);
+
+    }
+}
+
