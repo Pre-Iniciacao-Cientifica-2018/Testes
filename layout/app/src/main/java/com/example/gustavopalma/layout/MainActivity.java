5bd01bfa742aeb21eda433c1f325384a79e8ff86
Palmaaa <36799395+Palmaaa@users.noreply.github.com>
2018-12-24 11:20:10 -0200

    Protótipo do primeiro layout
    Apenas o layout em XML, sem nada funcional

---
+++ layout/app/src/main/java/com/example/gustavopalma/layout/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gustavopalma.layout;
+
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
