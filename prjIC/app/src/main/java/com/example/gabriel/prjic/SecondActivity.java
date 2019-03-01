dfea00cf5c836770bf4f9ff0bd5e3e969cc7f496
ederzed <42612795+ederzed@users.noreply.github.com>
2019-03-01 17:07:50 -0300

    Setas
    Agr tem setas com MotionEvent

--- prjIC/app/src/main/java/com/example/gabriel/prjic/SecondActivity.java
+++ prjIC/app/src/main/java/com/example/gabriel/prjic/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.gabriel.prjic;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,10 +12,20 @@ import android.support.v7.app.AppCompatActivity;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private LinearLayout lay = null;
+    private ImageView img = null;
+    private ImageView img1 = null;
+    //private float x,y;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +43,33 @@ public class SecondActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+        lay = (LinearLayout) findViewById(R.id.linear);
+        img = (ImageView) findViewById(R.id.imageView5);
+        img1 = (ImageView) findViewById(R.id.imageView9);
+        lay.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+            //    x = event.getX();
+            //    y = event.getY(); Um dia pode ser util
+                 if (event.getAction() == MotionEvent.ACTION_DOWN){
+                     if(img.getVisibility() == View.VISIBLE && img1.getVisibility() == View.VISIBLE){
+
+                         img.setVisibility(View.INVISIBLE);
+                         img1.setVisibility(View.INVISIBLE);
+                     }else{
+                         img.setVisibility(View.VISIBLE);
+                         img1.setVisibility(View.VISIBLE);
+                     }
+
+                 }
+
+                 return true;
+            }
+
+        });
     }
 
     @Override
@@ -92,4 +130,5 @@ public class SecondActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
